d8eeb07e8df58f3c0a05bb84bd725d5ea02373e5
mike777p <mjp7@duck.com>
2023-04-05 10:45:54 +0100

    Add pie chart final

--- budgetbuddy/components/Personal.jsx
+++ budgetbuddy/components/Personal.jsx
@@ -1,8 +1,4 @@
-<<<<<<< HEAD
-import { Text, Pressable, SafeAreaView, StyleSheet } from 'react-native';
-=======
 import { Text, Pressable, SafeAreaView, StyleSheet } from "react-native";
->>>>>>> fbe75dfedca8d9794558aa543fa83c6715f782f4
 
 const Personal = ({ navigation }) => {
   return (
@@ -11,11 +7,7 @@ const Personal = ({ navigation }) => {
         onPress={() => {
           navigation.navigate("Expense");
         }}
-<<<<<<< HEAD
-        
-=======
         title="Add Expense"
->>>>>>> fbe75dfedca8d9794558aa543fa83c6715f782f4
         style={styles.buttons}
       >
         <Text style={styles.buttonText}>Add Expense</Text>
@@ -24,50 +16,34 @@ const Personal = ({ navigation }) => {
         onPress={() => {
           navigation.navigate("Income");
         }}
-<<<<<<< HEAD
-=======
         title="Add Income"
->>>>>>> fbe75dfedca8d9794558aa543fa83c6715f782f4
         style={styles.buttons}
       >
         <Text style={styles.buttonText}>Add Income</Text>
       </Pressable>
       <Pressable
         onPress={() => {
-          navigation.navigate("SpendingStats");
+          navigation.navigate("Financial Stats");
         }}
-<<<<<<< HEAD
-=======
-        title="Spending Stats"
->>>>>>> fbe75dfedca8d9794558aa543fa83c6715f782f4
+        title="Financial Stats"
         style={styles.buttons}
       >
         <Text style={styles.buttonText}>Spending Stats</Text>
       </Pressable>
       <Pressable
         onPress={() => {
-<<<<<<< HEAD
-          navigation.navigate('Personal Goals');
-        }}
-=======
           navigation.navigate("Personal Goals");
         }}
         title="Set & View Goals"
->>>>>>> fbe75dfedca8d9794558aa543fa83c6715f782f4
         style={styles.buttons}
       >
         <Text style={styles.buttonText}>Set & View Goals</Text>
       </Pressable>
       <Pressable
         onPress={() => {
-<<<<<<< HEAD
-          navigation.navigate('Budget');
-        }}
-=======
           navigation.navigate("Budget");
         }}
         title="Set & View Budget"
->>>>>>> fbe75dfedca8d9794558aa543fa83c6715f782f4
         style={styles.buttons}
       >
         <Text style={styles.buttonText}>Set & View Budget</Text>
@@ -82,38 +58,16 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     // flexDirection: 'row',
-<<<<<<< HEAD
-    backgroundColor: '#080043',
-    alignItems: 'center',
-    justifyContent: 'space-evenly',
-    padding: 20,
-    borderTopWidth: 1,
-    borderColor: '#FC6C16',
-=======
     backgroundColor: "#080043",
     alignItems: "center",
     justifyContent: "space-evenly",
     padding: 20,
     borderTopWidth: 1,
     borderColor: "#FC6C16",
->>>>>>> fbe75dfedca8d9794558aa543fa83c6715f782f4
   },
   buttons: {
     flex: 0.1,
     borderWidth: 2,
-<<<<<<< HEAD
-    borderColor: '#00FFA6',
-    justifyContent: 'center',
-    alignContent: 'center',
-    alignItems: 'center',
-    borderRadius: 20,
-    height: 50,
-    width: 150,
-    backgroundColor: '#0800436f',
-  },
-  buttonText: {
-    color: '#f4f7f6',
-=======
     borderColor: "#00FFA6",
     justifyContent: "center",
     alignContent: "center",
@@ -125,7 +79,6 @@ const styles = StyleSheet.create({
   },
   buttonText: {
     color: "#f4f7f6",
->>>>>>> fbe75dfedca8d9794558aa543fa83c6715f782f4
     padding: 10,
     fontSize: 15,
     // justifyContent: 'center',
@@ -137,8 +90,4 @@ const styles = StyleSheet.create({
 // yellow: #F0F00F
 // pink: #EC20D8
 // orange: #FC6C16
-<<<<<<< HEAD
 // blue: #080043
-=======
-// blue: #080043
->>>>>>> fbe75dfedca8d9794558aa543fa83c6715f782f4
